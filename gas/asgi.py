8ad67dcbba7d75fda2287e4a85f7d9f619cb38ad
JamesCordova <jcordovas@unsa.edu.pe>
2022-08-04 17:27:16 -0500

    la carpeta del projecto se cambio de lugar

--- gas/asgi.py
+++ gas/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for gas1 project.
+ASGI config for gas project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gas1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gas.settings')
 
 application = get_asgi_application()
