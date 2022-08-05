8b4b0b8e3706d9dc1675d3f7a542cd8a61f4edea
888JordyEmanuelAymaCutipa888 <jayma@unsa.edu.pe>
2022-08-05 08:32:02 -0500

    creacion de la tabla

--- balones/models.py
+++ balones/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Balon(models.Model):
+
+    Lleno = models.BooleanField(default = True)
+    marcaDeEmpresa = models.CharField(max_length = 100)
+    Tipo = models.CharField(max_length = 100)
+
