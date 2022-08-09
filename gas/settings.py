6f78079968cccb4d4e396b5947737a589879fe04
Alejandro G <dgarciat@unsa.edu.pe>
2022-08-09 09:13:46 -0500

    conexion con t. trabajadores

--- gas/settings.py
+++ gas/settings.py
@@ -38,16 +38,24 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'trabajadores',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200"
 ]
 
 ROOT_URLCONF = 'gas.urls'
--- trabajadores/views.py
+++ trabajadores/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User,Group
+from rest_framework import viewsets
+from .serializer import TrabajadoresSerializer
+from .models import Trabajadores
 
 # Create your views here.
+class TrabajadoresViewSet(viewsets.ModelViewSet):
+    queryset = Trabajadores.objects.all()
+    serializer_class = TrabajadoresSerializer 
+
